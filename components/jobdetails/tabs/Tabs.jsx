ab862d8b816e29f8a1747bb3b63a2489e72bc492
georgeokez <georgeokez@gmail.com>
2023-11-27 23:32:07 +0000

    chore: rebuild Tabs component

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from "react";
-import { TouchableOpacity, FlatList, Text, View } from "react-native";
+import React, { useState } from 'react';
+import { TouchableOpacity, FlatList, Text, View } from 'react-native';
 
-import styles from "./tabs.style";
-import { SIZES } from "../../../constants";
+import styles from './tabs.style';
+import { SIZES } from '../../../constants';
 
 function TabButton({ name, activeTab, onHandleSearchType }) {
   return (
@@ -18,20 +18,7 @@ function TabButton({ name, activeTab, onHandleSearchType }) {
 const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
     <View style={styles.container}>
-      <FlatList
-        data={tabs}
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        renderItem={({ item }) => (
-          <TabButton
-            name={item}
-            activeTab={activeTab}
-            onHandleSearchType={() => setActiveTab(item)}
-          />
-        )}
-        contentContainerStyle={{ columnGap: SIZES.small / 2 }}
-        keyExtractor={(item) => item}
-      />
+      <Text>Job Tabs</Text>
     </View>
   );
 };
