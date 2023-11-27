8224371ee376f7889548b8165b0e17feccb8ae08
georgeokez <georgeokez@gmail.com>
2023-11-27 21:54:46 +0000

    feat: build out Nearbyjobs component

--- components/home/nearby/Nearbyjobs.jsx
+++ components/home/nearby/Nearbyjobs.jsx
@@ -1,47 +1,50 @@
-import React from "react";
 import { useRouter } from "expo-router";
 import { View, Text, TouchableOpacity, ActivityIndicator } from "react-native";
 
-// import styles from "./nearbyjobs.style";
-// import { COLORS } from "../../../constants";
-// import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
-// import useFetch from "../../../hook/useFetch";
+import styles from "./nearbyjobs.style";
+import { COLORS } from "../../../constants";
+import NearbyJobCard from "../../common/cards/nearby/NearbyJobCard";
+import { useFetch } from "../../../hooks/useFetch";
 
 const NearByJobs = () => {
   const router = useRouter();
-  // const { data, isLoading, error } = useFetch("search", {
-  //   query: "React Native developer",
-  //   num_pages: "1",
-  // });
+  const { data, isLoading, error } = useFetch("search", {
+    query: "React developer",
+    num_pages: "1",
+  });
+
+  const [selectedJob, setSelectedJob] = useState();
+
+  const handleCardPress = (item) => {
+    router.push(`/job-details/${item.job_id}`);
+    setSelectedJob(item.job_id);
+  };
 
   return (
-    <View>
-      <Text>NearByJobs Component</Text>
-    </View>
-    // <View style={styles.container}>
-    //   <View style={styles.header}>
-    //     <Text style={styles.headerTitle}>Nearby jobs</Text>
-    //     <TouchableOpacity>
-    //       <Text style={styles.headerBtn}>Show all</Text>
-    //     </TouchableOpacity>
-    //   </View>
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.headerTitle}>Nearby Jobs</Text>
+        <TouchableOpacity>
+          <Text style={styles.headerBtn}>Show all</Text>
+        </TouchableOpacity>
+      </View>
 
-    //   <View style={styles.cardsContainer}>
-    //     {isLoading ? (
-    //       <ActivityIndicator size="large" color={COLORS.primary} />
-    //     ) : error ? (
-    //       <Text>Something went wrong</Text>
-    //     ) : (
-    //       data?.map((job) => (
-    //         <NearbyJobCard
-    //           job={job}
-    //           key={`nearby-job-${job.job_id}`}
-    //           handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
-    //         />
-    //       ))
-    //     )}
-    //   </View>
-    // </View>
+      <View style={styles.cardsContainer}>
+        {isLoading ? (
+          <ActivityIndicator size="large" colors={COLORS.primary} />
+        ) : error ? (
+          <Text>Something went wrong</Text>
+        ) : (
+          data?.map((job) => (
+            <NearbyJobCard
+              job={job}
+              key={`nearby-job-${job?.job_id}`}
+              //handleNavigate={() => router.push(`/job-details/${job.job_id}`)}
+            />
+          ))
+        )}
+      </View>
+    </View>
   );
 };
 
