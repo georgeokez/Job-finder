452a5d6a8b17b231bec534dc4f2307aa0e821ff8
georgeokez <georgeokez@gmail.com>
2023-11-28 00:18:45 +0000

    chore: refactor

--- components/jobdetails/tabs/Tabs.jsx
+++ components/jobdetails/tabs/Tabs.jsx
@@ -1,24 +1,26 @@
 import React, { useState } from 'react';
-import { TouchableOpacity, FlatList, Text, View } from 'react-native';
+import { FlatList, View } from 'react-native';
+import TabButton from './TabButton';
 
 import styles from './tabs.style';
 import { SIZES } from '../../../constants';
 
-function TabButton({ name, activeTab, onHandleSearchType }) {
-  return (
-    <TouchableOpacity
-      style={styles.btn(name, activeTab)}
-      onPress={onHandleSearchType}
-    >
-      <Text style={styles.btnText(name, activeTab)}>{name}</Text>
-    </TouchableOpacity>
-  );
-}
-
 const Tabs = ({ tabs, activeTab, setActiveTab }) => {
   return (
     <View style={styles.container}>
-      <Text>Job Tabs</Text>
+      <FlatList
+        data={tabs}
+        renderItem={({ item }) => (
+          <TabButton
+            name={item}
+            activeTab={activeTab}
+            onHandleSearchType={() => setActiveTab(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        keyExtractor={(item) => item}
+      />
     </View>
   );
 };
